e0ed3a6fb8084cf180d831d68ca44df7fd4d2f71
Efo-Coder <efrahim.haser@hotmail.de>
2025-07-16 18:40:52 +0200

    list_utils.py added.

---
+++ list_utils.py
@@ -0,0 +1,7 @@
+def find_max(numbers):
+    """Findet die größte Zahl in einer Liste."""
+    return max(numbers)
+
+def remove_duplicates(items):
+    """Entfernt Duplikate aus einer Liste und gibt eine neue Liste zurück."""
+    return sorted(list(set(items)))
